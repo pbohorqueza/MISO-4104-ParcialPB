fcd16bde0c3caf159bbf37a89883cc565961411c
Paula Bohorquez <p.bohorqueza@uniandes.edu.co>
2023-11-05 22:49:58 -0500

    Añadida URL Base y ajustado el servicio

--- ventavehiculos/src/app/vehiculo/vehiculo.service.ts
+++ ventavehiculos/src/app/vehiculo/vehiculo.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Vehiculo } from './vehiculo';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable()
 export class VehiculoService {
+    private apiUrl = environment.baseUrl;
 
-constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
+
+    getCourses(): Observable<Vehiculo[]> {
+        return this.http.get<Vehiculo[]>(this.apiUrl);
+    }
+
 
 }
